3a64d76e5e43a5008fd7bdb27161e1b0eeeb3a96
TBoileau <t-boileau@email.com>
2022-10-04 13:28:42 +0200

    feat (sécurité): valider l'inscription (#29)

---
+++ src/UseCase/Security/ValidRegistrationInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UseCase\Security;
+
+use App\Entity\User;
+
+interface ValidRegistrationInterface
+{
+    public function __invoke(User $user): void;
+}
