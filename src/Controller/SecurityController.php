9bd4d9975da2fbf0d55352ee614fc35df421ef6e
TBoileau <t-boileau@email.com>
2022-10-04 10:54:20 +0200

    feat (securité): s'inscrire (#16)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\UseCase\Security\RegisterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -19,5 +23,33 @@ final class SecurityController extends AbstractController
             'last_username' => $authenticationUtils->getLastUsername(),
             'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, RegisterInterface $register): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $register($user);
+
+            $this->addFlash('success', 'Votre compte a bien été créé.');
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->renderForm('security/register.html.twig', ['form' => $form, 'user' => $user]);
+    }
+
+    #[Route('/register/{registrationToken}/valid', name: 'valid_registration')]
+    public function validRegistration(): void
+    {
     }
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'empty_data' => '',
+                'attr' => [
+                    'placeholder' => 'Votre adresse email',
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'empty_data' => '',
+                'attr' => [
+                    'placeholder' => 'Votre mot de passe',
+                ],
+            ])
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo',
+                'empty_data' => '',
+                'attr' => [
+                    'placeholder' => 'Votre pseudo',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('data_class', User::class);
+        $resolver->setDefault('validation_groups', ['registration', 'Default']);
+    }
+}
