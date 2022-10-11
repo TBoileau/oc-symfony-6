637ebccc61462fbadc66c0a4a22b1af0fb3c4004
TBoileau <t-boileau@email.com>
2022-10-11 13:17:53 +0200

    feat (figure): option mise à jour d'une figure seulement par l'auteur (#44)

--- src/Security/Voter/TrickVoter.php
+++ src/Security/Voter/TrickVoter.php
@@ -9,17 +9,22 @@ use App\Doctrine\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
+use function in_array;
+
 final class TrickVoter extends Voter
 {
     public const DELETE = 'delete';
+    public const UPDATE = 'update';
 
-    public function __construct(private readonly bool $deleteTrickByOwnerOnly)
-    {
+    public function __construct(
+        private readonly bool $deleteTrickByOwnerOnly,
+        private readonly bool $updateTrickByOwnerOnly
+    ) {
     }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return self::DELETE === $attribute && $subject instanceof Trick;
+        return in_array($attribute, [self::DELETE, self::UPDATE], true) && $subject instanceof Trick;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -33,6 +38,10 @@ final class TrickVoter extends Voter
         /** @var Trick $trick */
         $trick = $subject;
 
-        return !$this->deleteTrickByOwnerOnly || $trick->getUser() === $user;
+        return match ($attribute) {
+            self::DELETE => !$this->deleteTrickByOwnerOnly || $trick->getUser() === $user,
+            self::UPDATE => !$this->updateTrickByOwnerOnly || $trick->getUser() === $user,
+            default => false,
+        };
     }
 }
